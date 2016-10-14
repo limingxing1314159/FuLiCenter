3594f42c9a29e069558120364f0612ef03ed7ac6
limingxing1314159 <1473243403@qq.com>
2016-10-14 15:59:35 +0800

    添加跳转工具类，并在闪屏中实现

--- app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.utils.MFGT;
 
 public class SplashActivity extends AppCompatActivity {
     private final long sleepTime = 2000;
@@ -32,7 +33,8 @@ public class SplashActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                MFGT.gotoMainActivity(SplashActivity.this);
+                MFGT.finish(SplashActivity.this);
             }
         }).start();
 
