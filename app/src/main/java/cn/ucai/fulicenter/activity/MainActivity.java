9f7027967a12079a77d83605b696e9158d970f1b
limingxing1314159 <1473243403@qq.com>
2016-10-17 20:15:48 +0800

    butterknife架包的安装及使用，RadioButton的互斥实现

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -1,15 +1,74 @@
 package cn.ucai.fulicenter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Bind(R.id.btnNewGoods)
+    RadioButton mbtnNewGoods;
+    @Bind(R.id.btnBoutique)
+    RadioButton mbtnBoutique;
+    @Bind(R.id.btnCategory)
+    RadioButton mbtnCategory;
+    @Bind(R.id.layoutCart)
+    RadioButton mlayoutCart;
+    @Bind(R.id.tvCartHint)
+    TextView mtvCartHint;
+    @Bind(R.id.btnMe)
+    RadioButton mbtnMe;
+
+    int index;
+    RadioButton[] rbs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initView();
+    }
+    private void initView() {
+        rbs = new RadioButton[5];
+        rbs[0] = mbtnNewGoods;
+        rbs[1] = mbtnBoutique;
+        rbs[2] = mbtnCategory;
+        rbs[3] = mlayoutCart;
+        rbs[4] = mbtnMe;
+    }
+    public void onCheckedChange(View v){
+        switch (v.getId()){
+            case R.id.btnNewGoods:
+                index = 0;
+                break;
+            case R.id.btnBoutique:
+                index = 1;
+                break;
+            case R.id.btnCategory:
+                index = 2;
+                break;
+            case R.id.layoutCart:
+                index = 3;
+                break;
+            case R.id.btnMe:
+                index = 4;
+                break;
+        }
+        setRadioButtonStatus();
+    }
+    private void setRadioButtonStatus() {
+        for (int i = 0;i<rbs.length;i++){
+            if (i==index){
+                rbs[i].setChecked(true);
+            }else {
+                rbs[i].setChecked(false);
+            }
+        }
     }
 }
