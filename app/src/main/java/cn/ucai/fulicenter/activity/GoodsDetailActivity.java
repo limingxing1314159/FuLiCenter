ea9bb7d49a391136c4d2e4948532838b82aadd3e
limingxing1314159 <1473243403@qq.com>
2016-10-19 09:00:55 +0800

    商品详情页面布局实现以及数据的填充

--- app/src/main/java/cn/ucai/fulicenter/activity/GoodsDetailActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/GoodsDetailActivity.java
@@ -1,19 +1,129 @@
 package cn.ucai.fulicenter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.AlbumsBean;
+import cn.ucai.fulicenter.bean.GoodsDetailsBean;
+import cn.ucai.fulicenter.net.NetDao;
+import cn.ucai.fulicenter.net.OkHttpUtils;
+import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.L;
+import cn.ucai.fulicenter.utils.MFGT;
+import cn.ucai.fulicenter.view.FlowIndicator;
+import cn.ucai.fulicenter.view.SlideAutoLoopView;
 
 public class GoodsDetailActivity extends AppCompatActivity {
 
+    @Bind(R.id.backClickArea)
+    LinearLayout backClickArea;
+    @Bind(R.id.tv_good_name_english)
+    TextView tvGoodNameEnglish;
+    @Bind(R.id.tv_good_name)
+    TextView tvGoodName;
+    @Bind(R.id.tv_good_price_shop)
+    TextView tvGoodPriceShop;
+    @Bind(R.id.tv_good_price_current)
+    TextView tvGoodPriceCurrent;
+    @Bind(R.id.salv)
+    SlideAutoLoopView salv;
+    @Bind(R.id.indicator)
+    FlowIndicator indicator;
+    @Bind(R.id.layout_image)
+    RelativeLayout layoutImage;
+    @Bind(R.id.wv_good_brief)
+    WebView wvGoodBrief;
+
+    int goodsId;
+    GoodsDetailActivity mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_detail);
-        int goodsID = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID,0);
-        L.e("detail","goodsid="+goodsID);
+        ButterKnife.bind(this);
+         goodsId = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID, 0);
+        L.e("detail", "goodsid=" + goodsId);
+        if (goodsId==0){
+            finish();
+        }
+        mContext = this;
+        initView();
+        initData();
+        setListener();
+    }
+
+    private void setListener() {
+
+
+    }
+
+    private void initData() {
+        NetDao.downloadGoodsDetail(mContext, goodsId, new OkHttpUtils.OnCompleteListener<GoodsDetailsBean>() {
+            @Override
+            public void onSuccess(GoodsDetailsBean result) {
+                L.i("details="+result);
+                if (result!=null){
+                    showGoodsDetails(result);
+                }else {
+                    finish();
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                finish();
+                L.e("details,error="+error);
+                CommonUtils.showShortToast(error);
+            }
+        });
+    }
+
+    private void showGoodsDetails(GoodsDetailsBean details) {
+        tvGoodNameEnglish.setText(details.getGoodsEnglishName());
+        tvGoodName.setText(details.getGoodsName());
+        tvGoodPriceCurrent.setText(details.getCurrencyPrice());
+        tvGoodPriceShop.setText(details.getShopPrice());
+        salv.startPlayLoop(indicator,getAlbumImgUrl(details),getAlbumImgCount(details));
+        wvGoodBrief.loadDataWithBaseURL(null,details.getGoodsBrief(),I.TEXT_HTML,I.UTF_8,null);
+    }
+
+    private int getAlbumImgCount(GoodsDetailsBean details) {
+        if (details.getProperties()!=null && details.getProperties().length>0) {
+            return details.getProperties()[0].getAlbums().length;
+        }
+        return 0;
+    }
+
+    private String[] getAlbumImgUrl(GoodsDetailsBean details) {
+        String[] urls = new String[]{};
+        if (details.getProperties()!=null && details.getProperties().length>0){
+            AlbumsBean[] album = details.getProperties()[0].getAlbums();
+            urls = new String[album.length];
+            for (int i=0;i<album.length;i++){
+                urls[i] = album[i].getImgUrl();
+            }
+        }
+        return urls;
+    }
+
+    private void initView() {
+
+    }
+    @OnClick(R.id.backClickArea)
+    public void onBackClick(){
+        MFGT.finish(this);
+    }
+    public void onback(View v){
+        MFGT.finish(this);
     }
 }
