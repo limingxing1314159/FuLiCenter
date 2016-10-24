9b942945cb2a29961e96c857c0b3297ac0ee348f
limingxing1314159 <1473243403@qq.com>
2016-10-24 17:37:05 +0800

    添加sharepreference保存用户名

--- app/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
@@ -12,10 +12,13 @@ import android.widget.EditText;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.Result;
 import cn.ucai.fulicenter.bean.User;
+import cn.ucai.fulicenter.dao.SharePrefrenceUtils;
+import cn.ucai.fulicenter.dao.UserDao;
 import cn.ucai.fulicenter.net.NetDao;
 import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.L;
@@ -100,7 +103,15 @@ public class LoginActivity extends BaseActivity {
                     if (result.isRetMsg()){
                         User user = (User) result.getRetData();
                         L.e(TAG,"User = "+user);
-                        MFGT.finish(mContent);
+                        UserDao dao = new UserDao(mContent);
+                        boolean isSuccess = dao.saveUser(user);
+                        if (isSuccess){
+                            SharePrefrenceUtils.getInstance(mContent).saveUser(user.getMuserName());
+                            FuLiCenterApplication.setUser(user);
+                            MFGT.finish(mContent);
+                        }else {
+                            CommonUtils.showLongToast(R.string.user_database_error);
+                        }
                     }else {
                         if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER){
                             CommonUtils.showLongToast(R.string.login_fail_unknow_user);
