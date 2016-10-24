a467636758d953d2282aa874b315fa23b1306cf4
limingxing1314159 <1473243403@qq.com>
2016-10-24 16:25:15 +0800

    添加数据库操作的三个工具类

--- app/src/main/java/cn/ucai/fulicenter/dao/DBOpenHelper.java
+++ app/src/main/java/cn/ucai/fulicenter/dao/DBOpenHelper.java
@@ -12,11 +12,19 @@ import cn.ucai.fulicenter.I;
 public class DBOpenHelper extends SQLiteOpenHelper{
     private static final int DATABASE_VERSION = 1;
     private static DBOpenHelper instance;
-    private static final String CREATE_USER_TABLE = "";
+    private static final String FULICENTER_USER_TABLE_CREATE = "CREATE TABLE "
+            + UserDao.USER_TABLE_NAME + " ("
+            + UserDao.USER_COLUMN_NAME + " TEXT PRIMARY KEY, "
+            + UserDao.USER_COLUMN_NICK + " TEXT, "
+            + UserDao.USER_COLUMN_AVATAR_ID + " INTEGER, "
+            + UserDao.USER_COLUMN_AVATAR_TYPE + " INTEGER, "
+            + UserDao.USER_COLUMN_AVATAR_PATH + " TEXT, "
+            + UserDao.USER_COLUMN_AVATAR_SUFFIX + " TEXT, "
+            + UserDao.USER_COLUMN_AVATAR_LASTUPDATE_TIME + " TEXT);";
 
-    public static DBOpenHelper onInit(Context context){
+    public static DBOpenHelper getInstance(Context context){
         if (instance==null){
-            instance = new DBOpenHelper(context);
+            instance = new DBOpenHelper(context.getApplicationContext());
         }
         return instance;
     }
@@ -30,7 +38,7 @@ public class DBOpenHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(CREATE_USER_TABLE);
+        sqLiteDatabase.execSQL(FULICENTER_USER_TABLE_CREATE);
     }
 
     @Override
@@ -40,7 +48,8 @@ public class DBOpenHelper extends SQLiteOpenHelper{
 
     public void closeDB(){
         if (instance!=null){
-            instance.close();
+            SQLiteDatabase db = instance.getWritableDatabase();
+            db.close();
             instance = null;
         }
     }
--- app/src/main/java/cn/ucai/fulicenter/dao/UserDao.java
+++ app/src/main/java/cn/ucai/fulicenter/dao/UserDao.java
@@ -8,26 +8,26 @@ import cn.ucai.fulicenter.bean.User;
  * Created by Administrator on 2016/10/24.
  */
 public class UserDao {
-    public static final String TABLE_USER_NAME = "t_superwechat_user";
-    public static final String TABLE_COLUMN_NAME = "m_user_name";
-    public static final String TABLE_COLUMN_NICK = "m_user_nick";
-    public static final String TABLE_COLUMN_AVATAR_ID = "m_user_avatar_id";
-    public static final String TABLE_COLUMN_AVATAR_TYPE = "m_user__avatar_type";
-    public static final String TABLE_COLUMN_AVATAR_PATH = "m_user_avatar_path";
-    public static final String TABLE_COLUMN_AVATAR_SUFFIX = "m_user_avatar_suffix";
-    public static final String TABLE_COLUMN_AVATAR_LASTUPDATE_TIME = "m_user_avatar_lastupdate_time";
-    DBManager dbManager;
+    public static final String USER_TABLE_NAME = "t_superwechat_user";
+    public static final String USER_COLUMN_NAME = "m_user_name";
+    public static final String USER_COLUMN_NICK = "m_user_nick";
+    public static final String USER_COLUMN_AVATAR_ID = "m_user_avatar_id";
+    public static final String USER_COLUMN_AVATAR_TYPE = "m_user__avatar_type";
+    public static final String USER_COLUMN_AVATAR_PATH = "m_user_avatar_path";
+    public static final String USER_COLUMN_AVATAR_SUFFIX = "m_user_avatar_suffix";
+    public static final String USER_COLUMN_AVATAR_LASTUPDATE_TIME = "m_user_avatar_lastupdate_time";
+
     public UserDao(Context context) {
-        dbManager = DBManager.onInit(context);
+        DBManager.getInstance().onInit(context);
     }
     public boolean saveUser(User user){
-        return dbManager.saveUser(user);
+        return DBManager.getInstance().saveUser(user);
     }
     public User getUser(String username){
-        return dbManager.getUser(username);
+        return DBManager.getInstance().getUser(username);
     }
     public boolean updateUser(User user){
-        return dbManager.updateUser(user);
+        return DBManager.getInstance().updateUser(user);
     }
 }
 
